b522fcda42ffd2e10176612bdad3df978475c7d5
bennywu2019 <benny.wu.new@gmail.com>
2021-07-26 18:25:05 -0400

    updated files

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import math
 import credential as crt
 from database import *
 
-# Open connection
+# Open connection yayy
 db = Database(crt.host, crt.db_name, crt.user, crt.password)
 result = db.query(""" SELECT * FROM "User" """)
 print(result)

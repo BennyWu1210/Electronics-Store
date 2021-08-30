e4b8cb102ad7bd8ea541216101285864800fc4fd
bennywu2019 <benny.wu.new@gmail.com>
2021-08-30 19:05:29 -0400

    improve admin feature

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
-import datetime
 import math
-import login
+import _sha256 as hash
+
+from datetime import datetime
 from product import Product
 from product import select_product
 from user import User
+from user import Admin
+from transaction import Purchase
+from transaction import Invoice
 
 import credential as crt
 from database import *
@@ -14,6 +18,7 @@ from database import *
 #  2. User (user.py) --> let admin view information
 #  3. Transaction history (transaction.py, or something like that)
 #  4. Store (store.py) --> add user, keep track of everything
+#  5. Admin: have access to products, users, transaction.
 #  Make the files for the classes first
 
 
@@ -21,9 +26,6 @@ from database import *
 
 db = Database(crt.host, crt.db_name, crt.user, crt.password)
 result = db.query(""" SELECT * FROM "User" """)
-
-new_user = login.login("user1", "password1", db)
-username = "user1"
 
 
 def create_user(user_info, username):
@@ -34,13 +36,41 @@ def create_user(user_info, username):
     age = user_info[4]
     address = user_info[5].rstrip() if type(user_info[5]) is str else None
 
+    admin_info = get_admin(user_id)
+
+    if len(admin_info) > 0:
+        admin = Admin(int(admin_info[0][0]), user_id, username, first_name, last_name, email, age, address)
+        return admin
+
     user = User(user_id, username, first_name, last_name, email, age, address)
     return user
 
 
-def create_product():
-    pass
+def get_admin(user_id):
+    query = """SELECT admin_id FROM "Admin" WHERE user_id = """ + user_id + ";"
+    result = db.query(query)
+    return result
 
+
+def login(username, password):
+    # password need hashing
+    query = """
+        SELECT * FROM "User" WHERE user_id = (SELECT user_id 
+        FROM "Login" WHERE username = \'""" \
+        + str(username) + """\' AND password = \'""" \
+        + str(password) + """\');"""
+
+    result = db.query(query)
+
+    if not result:  # if it is not empty
+        return None
+    else:
+        user = result[0]
+        return user
+
+
+new_user = login("user1", "password1")
+username = "user1"
 
 # Create user testing
 # print(create_user(new_user, username))
@@ -51,4 +81,9 @@ apple = Product(1, 2, 3, 4, 5, 6, 7)
 
 print(select_product(db, 1, "ben", 32.1, 10, "phone", 2022, "apple")[0])
 
-# hihihihi testing 01
+
+purchase = Purchase(apple, 10)
+invoice = Invoice(new_user, datetime.now())
+invoice.add_purchase(purchase)
+
+print(invoice.transactions[0].quantity)
--- user.py
+++ user.py
@@ -8,10 +8,15 @@ class User:
         self._email = email
         self._address = address
         self.age = age
+        self._isAdmin = False
+        self.cart = []  # stores tuple => (product_id, quantity)
 
     def __str__(self):
         return "User " + str(self.username) + ": id " + str(self.user_id) + ", name " + \
                self.first_name + " " + self.last_name + ", email " + self.email
+
+    def add_item(self, product_id, quantity):
+        self.append((product_id, quantity))
 
     @property
     def user_id(self):
@@ -33,6 +38,13 @@ class User:
     def email(self):
         return self._email
 
+
+# Left out customer id as of now
+class Admin(User):
+    def __init__(self, admin_id, user_id, username, first_name, last_name, email, age=None, address=None):
+        self.admin_id = admin_id
+        User.__init__(user_id, username, first_name, last_name, email, age, address)
+        self._isAdmin = True
 
 
 user = User(1, "guy", "dude", "guy", "dude@guy.com")
